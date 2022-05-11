a6c6d86df9b04b6cbd0e5d3b2a09ab978bb15b73
mpadillazeravla <mikepadilla82@gmail.com>
2022-05-11 18:25:38 +0000

    add 4th loop

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+//import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//write your code here
+// console.log("Hello Rigo from the console!");
+//};
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "white"];
+let noun = ["jogger", "racoon", "website"];
+let extension = [".com", ".es", ".org"];
+
+// Hacemos un bucle anidado para hacer console.log de todas las posibilidades
+let domain = "";
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        domain = pronoun[i] + adj[j] + noun[k] + extension[l];
+        console.log(domain);
+      }
+    }
+  }
+}
